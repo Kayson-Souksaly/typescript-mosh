0a818868733e073c5dc95a7114f2fa86054f3faa
Kayson-Souksaly <kayson.souksaly@gmail.com>
2025-08-21 23:16:37 -0400

    Learning Union types and narrowing.

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 2] = "Small";
-    Size[Size["Medium"] = 3] = "Medium";
-    Size[Size["Large"] = 4] = "Large";
-})(Size || (Size = {}));
-let mySize = Size.Medium;
-console.log(mySize);
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs("10");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-type Employee = {
-  readonly id: number;
-  name: string;
-  retire: (date: Date) => void;
-};
+function kgToLbs(weight: number | string): number {
+  // weight. <--- Can only see properties and methods common to both types number and string (e.g. toLocaleString, toString, valueOf)
+  // Narrowing the type to number
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+}
 
-let employee: Employee = {
-  id: 1,
-  name: "Kayson",
-  retire: (date: Date) => console.log(date),
-};
+// Usage examples
+kgToLbs(10); // 22
+kgToLbs("10"); // 22
